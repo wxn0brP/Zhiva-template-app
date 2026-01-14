e1c82ddcfdc12197b6fe4ba08d8a79bdc7cdfef9
wxn0brP <toja.filiper@gmail.com>
2026-01-14 09:53:17 +0100

    feat: improve

--- src/browser.ts
+++ src/browser.ts
@@ -1,1 +1,11 @@
 import "@wxn0brp/flanker-ui/html";
+import { fetchApi, fetchApiJson, fetchApiPost } from "@wxn0brp/zhiva-base-lib/front/api";
+
+// fetch secure api and parse json
+fetchApiJson("hello").then(console.log);
+
+// fetch secure api
+fetchApi("hello").then(res => res.text()).then(console.log);
+
+// post secure api
+fetchApiPost("hello", { msg: "Hello from Zhiva" }).then(console.log);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import { app, oneWindow } from "@wxn0brp/zhiva-base-lib";
+import { apiRouter } from "@wxn0brp/zhiva-base-lib/api";
 
+// like express.static
+// app.static("/endpoint", "/path/to/directory");
+// or
+// app.static("endpoint + directory path");
 app.static("public");
 app.static("dist");
 
+// Define a secure API endpoint
+apiRouter.get("/hello", async () => {
+    return { err: false, msg: "Hello world!" };
+});
+
+// Define a POST API endpoint
+apiRouter.post("/hello", async (req) => {
+    console.log(req.body.hello);
+    return { err: false, msg: req.body.hello };
+});
+
+// Create the application window
+// If window closed, the process will exit
 oneWindow();
